b8881c8f4477e1e807de6ded903bf06f778ec0de
Prasanth-Rc <prasanth98780@gmail.com>
2025-05-15 11:10:43 +0530

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import './../styles/App.css';
 
 const App = () => {
+  const relatives = [
+    "Uncle John",
+    "Aunt Mary",
+    "Cousin David",
+    "Grandma Smith",
+    "Grandpa Wilson",
+    "Sister Emily",
+    "Brother Michael"
+  ];
+
   return (
-    <div id="main">
-               {/* Do not remove the main div */}
+    <div>
+      {/* Do not remove the main div */}
+      <h2>Diwali Relative Visit List</h2>
+      <ol key="relativeList" className="relative-list">
+        {relatives.map((relative, index) => (
+          <li 
+            key={`relativeListItem${index + 1}`}
+            className="relative-item"
+          >
+            {relative}
+          </li>
+        ))}
+      </ol>
     </div>
   )
 }
 
-export default App
+export default App;
